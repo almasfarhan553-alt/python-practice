4a549f3fe4c2e13d9780466cdc1b52732f76e179
ALMAS FARHAN <almasfarhan553@gmail.com>
2026-06-09 17:58:25 +0530

    Added barnd name generator code3

---
+++ musicalsetupname.py
@@ -0,0 +1,4 @@
+#with the help of your city name and your pet name we can make a brand name
+city = input("What is Your City Name\nAns:- ")
+pet = input("What is Your Pet Name\nAns:- ")
+print("I can make Your Brand name,\nAnd Your Brand Name is " + city + (" ") + pet + (".") )
